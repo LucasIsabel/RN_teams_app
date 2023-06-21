349a4cd786bbbb56000a18a4beab86e1cb75fb60
lucky.o.isa@gmail.com <Lucas Isabel>
2023-06-20 23:47:29 -0300

    feat: added google fonts into application

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,5 @@
-import { Text } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
-  return (
-    <Container>
-      <Text> HELLO WORLD </Text>
-    </Container>
-  );
+  return <Container></Container>;
 }
